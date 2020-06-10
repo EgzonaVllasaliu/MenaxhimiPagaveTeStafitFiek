5595f1bb632f0e4565224f9f6f88031eb55294f8
PuhizaDoci <puhize.doci@studentet.uni-pr.edu>
2020-06-10 21:40:19 +0200

    Paraqitja e chat
    Shtimi i files, qasja ne javascript files ka problem
    
    Co-Authored-By: Rrezarta Sallauka <rrezartasallauka@users.noreply.github.com>

---
+++ models/Chat.js
@@ -0,0 +1,11 @@
+class Chat {
+    // todo add other user model attributes
+    constructor({ id, message, senderId }) {
+        this.id = id;
+        this.message = message;
+        this.senderId=senderId;
+    }
+  }
+  // module.exports = User;
+  // let Chat = pool.model("theChat", chatSchema);
+  module.exports = Chat;
